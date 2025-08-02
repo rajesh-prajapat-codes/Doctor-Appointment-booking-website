92d96d1540c6b13a201c88c909d95247e2c9d218
rajesh-prajapat-codes <rajeshprajapat0108@gmail.com>
2025-08-02 11:08:30 +0530

    second commit

--- Doctor-Appointment/src/components/Navbar.jsx
+++ Doctor-Appointment/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import adminLogo from '../assets/assets_admin/admin_logo.svg';
-import {NavLink} from 'react-router-dom'
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
+import { NavLink, useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false)
+  const [token , setToken] = useState(true)
   return (
     <div className='flex item-center justify-between text-sm py-4 mb-5 border-b-gray-400'>
-      <img className='w-44 cursor-pointer' src={adminLogo} alt="Admin Logo" />
+      <img className='w-44 cursor-pointer' src={assets.logo} alt="Admin Logo" />
       <ul className='hidden md:flex items-start gap-5 font-medium '>
         <NavLink to= '/' >
           <li className='py-1'>Home</li>
@@ -25,7 +28,22 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
-        <button className='bg-purple-500 text-white px-8 py-3 rounded-full font-light hidden md:block '>Create Acount</button>
+        {
+          token
+            ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+              <img className='w-8 rounded full' src={assets.profile_pic} alt="" />
+              <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                  <p className='hover:text-black cursor-pointer'>My Profile</p>
+                  <p className='hover:text-black cursor-pointer'>My Appointments</p>
+                  <p className='hover:text-black cursor-pointer'>Logout</p>
+                </div>
+              </div>
+            </div>
+            :<button onClick={() => navigate('/login')} className='bg-purple-500 text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer '>Create Acount</button>
+        }
+        
       </div>
     </div>
   )
